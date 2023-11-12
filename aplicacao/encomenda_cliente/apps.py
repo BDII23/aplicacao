77197f6795c0c74b56cd1d4b3ac757672513c10f
Lucas Sebastião <75097274+lmsebastiao@users.noreply.github.com>
2023-11-12 07:51:10 +0000

    Criação da aplicação

---
+++ aplicacao/encomenda_cliente/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EncomendaClienteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'encomenda_cliente'
---
+++ aplicacao/encomenda_fornecedor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EncomendaFornecedorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'encomenda_fornecedor'
