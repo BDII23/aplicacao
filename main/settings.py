5393acc0321c9d14d2c34b263c3da5cb48ea1513
JoaoSantos12f <131891283+JoaoSantos12f@users.noreply.github.com>
2023-12-06 09:48:58 +0000

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bdii', 
         'USER': 'postgres',
-        'PASSWORD': '6331',
+        'PASSWORD': '6331', #aqui é diferente para cada um 
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     },
