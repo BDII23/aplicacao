80dff127bea7b8fc690558cb0299f687f7309cd4
Makal <miguelpmarques2602@gmail.com>
2024-01-08 11:34:00 +0000

    Tentei. Não sei por onde começar

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from . import views
+from . import views 
+from .views import atualizar_componentes, listar_componentes
+
 
 urlpatterns = [
+    # path('', pagina_inicial, name='pagina_inicial'),
+    path('/atualizar_componentes', atualizar_componentes, name='atualizar_componentes'),
+    path('listar_componentes/', listar_componentes, name='listar_componentes'),
  ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Componente
+
+def listar_componentes(request):
+    componentes = Componente.objects.all()
+    return render(request, 'templates/componentes/listar.html', {'componentes': componentes})
+
+def atualizar_componentes(request):
+    componentes = Componente.objects.all()
+    return render(request, 'templates/componentes/atualizar.html', {'componentes': componentes})
+
 
 # Create your views here.
