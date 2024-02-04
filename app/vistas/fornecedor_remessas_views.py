817a5512a52c7e80a173a06d7346d9ebeaa08229
Makal <miguelpmarques2602@gmail.com>
2024-02-04 18:16:27 +0000

    Yo apaguei aqui umas pastas e fiz o listar de guia remessa dos clientes e fornecedores

--- app/vistas/fornecedor_remessas_views.py
+++ app/vistas/fornecedor_remessas_views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from ..database.guia_remessa_fornecedor import *
 
 
-def fornecedores_remessas_listar():
-	return
+def fornecedores_remessas_listar(request):
+    try:
+        guia_remessa = readjson_guia_remessa_fornecedor()
+        print(guia_remessa)
+        return render(request, 'fornecedor_remessas/listar.html', {'guia_remessa': guia_remessa})
+    except Exception as e:
+        return HttpResponse(e)
 
-
-def fornecedores_remessas_listar_id():
-	return
-
+def fornecedores_remessas_listar_id(request, id):
+    try:
+        remessa = readone_guia_remessa_fornecedor(id)
+        return render(request, 'fornecedor_remessas/listar_id.html', {'remessa': remessa})
+    except Exception as e:
+        return HttpResponse(e)
 
 def fornecedores_remessas_registar():
 	return
